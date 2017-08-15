e47b3e154f70e098f8f1d7345a45d5b0558a16b2
Douglas Wilson <douglas.wilson@gmail.com>
2017-08-16 09:23:22 +1200

    Add TypeInType language pragma to Foundation.Primitive..Error
    This was required to compile on ghc head

--- Foundation/Primitive/Error.hs
+++ Foundation/Primitive/Error.hs
@@ -3,6 +3,9 @@
 {-# LANGUAGE ImplicitParams #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ >= 800
+{-# LANGUAGE TypeInType #-}
+#endif
 module Foundation.Primitive.Error
     ( error
     ) where
